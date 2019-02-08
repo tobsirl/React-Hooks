91796e14f8336db39deb77ea2f5c7d70740a0935
Paul Tobin <tobster111@gmail.com>
2019-02-08 12:35:11 +0000

    Basic Todo app setup

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Todo = props => {
+  return (
+    <React.Fragment>
+      <h3>Todo</h3>
+      <input type="text" placeholder="Todo"/>
+        <button type="button">Add</button>
+      <ul>
+        
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default Todo;
