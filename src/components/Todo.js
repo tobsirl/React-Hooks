4ba4ed4a715e6c2dc8fea60d9de0df96bcd1c293
Paul Tobin <tobster111@gmail.com>
2019-02-08 13:26:00 +0000

    Using useState

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Todo = props => {
+  const inputState = useState('');
+
+  const inputChangeHandler = event => {
+    inputState[1](event.target.value);
+  };
+
   return (
     <React.Fragment>
       <h3>Todo</h3>
-      <input type="text" placeholder="Todo"/>
-        <button type="button">Add</button>
-      <ul>
-        
-      </ul>
+      <input
+        type="text"
+        placeholder="Todo"
+        onChange={inputChangeHandler}
+        value={inputState[0]}
+      />
+      <button type="button">Add</button>
+      <ul />
     </React.Fragment>
   );
 };
