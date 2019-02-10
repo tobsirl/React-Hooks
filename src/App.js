83bff7fcd741e4c7ada8aebaffcc4c4fd089fb7b
Paul Tobin <tobster111@gmail.com>
2019-02-10 10:14:38 +0000

    Added two components, Header.js + Auth.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import Todo from './components/Todo';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Todo />
       </div>
     );
