4fa78e8f0fa6683119f8564ced0be10f9820b855
Paul Tobin <tobster111@gmail.com>
2019-02-12 11:32:09 +0000

    Added a list component to rendered the ul

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const list = props => {
+  console.log('Rendering the list...');
+  return (
+    <ul>
+      {props.items.todoList.map(todo => (
+        <li key={todo.id} onClick={props.onClick.bind(this, todo.id)}>
+          {todo.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default list;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -116,13 +116,7 @@ const Todo = props => {
       <button type="button" onClick={todoClearHandler}>
         Clear
       </button>
-      <ul>
-        {todoList.map(todo => (
-          <li key={todo.id} onClick={todoRemoveHandler.bind(this, todo.id)}>
-            {todo.name}
-          </li>
-        ))}
-      </ul>
+      
     </React.Fragment>
   );
 };
